5bc43a52d2f528c156059f651c4ae6608156d6d7
Fart1k <seinartur@gmail.com>
2025-09-04 23:10:07 +0300

    Update Student.cs

--- TallinnaRakenduslikKolledz/Models/Student.cs
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -8,8 +8,9 @@ namespace TallinnaRakenduslikKolledz.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-       
         public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+        
         public DateTime? BirthDate { get; set; }
     }
 }
