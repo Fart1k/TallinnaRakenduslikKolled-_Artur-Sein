3f14576976a6fe94804f77af2efa0b2e0da8446f
Fart1k <seinartur@gmail.com>
2025-09-04 14:03:02 +0300

    3

---
+++ TallinnaRakenduslikKolledz/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Data
+{
+    public class SchoolContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
