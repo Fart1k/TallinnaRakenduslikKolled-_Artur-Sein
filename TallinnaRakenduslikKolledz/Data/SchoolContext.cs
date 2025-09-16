35550f38e72eb452d6d7cd1280eb114a8732f40c
Fart1k <seinartur@gmail.com>
2025-09-16 09:22:23 +0300

    Update SchoolContext.cs

--- TallinnaRakenduslikKolledz/Data/SchoolContext.cs
+++ TallinnaRakenduslikKolledz/Data/SchoolContext.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Models;
 using TallinnaRakenduslikKolledz.Models;
 
 namespace TallinnaRakenduslikKolledz.Data
@@ -10,12 +12,18 @@ namespace TallinnaRakenduslikKolledz.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
         }
     }
 }
