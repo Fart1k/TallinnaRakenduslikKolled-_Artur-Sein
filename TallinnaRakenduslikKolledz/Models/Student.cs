088b18a479768cc11948a28ef512e692e5e16fc2
artur <artur@HOME-PC>
2025-09-18 00:26:32 +0300

    working Edit

--- TallinnaRakenduslikKolledz/Models/Student.cs
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -8,12 +8,12 @@ namespace TallinnaRakenduslikKolledz.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
         public ICollection<Enrollment>? Enrollments { get; set; }
         
         public DateTime? BirthDate { get; set; }
 
-        public string email { get; set; }
+        public string? email { get; set; }
 
         public int? Age { get; set; }
         public string? Gender { get; set; }
