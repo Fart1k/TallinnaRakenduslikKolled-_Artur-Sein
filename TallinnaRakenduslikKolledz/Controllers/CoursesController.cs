ac729f0ad894d107394f6e6e4ae1b0a9b2c5b0a4
opilane <opilane@HP-CZC321706P>
2025-09-29 14:13:34 +0300

    added createpost

--- TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
+++ TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Formats.Asn1;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
+using TallinnaRakenduslikKolledz.Models;
 
 namespace TallinnaRakenduslikKolledz.Controllers
 {
@@ -18,12 +21,59 @@ namespace TallinnaRakenduslikKolledz.Controllers
                 .AsNoTracking();
             return View(courses);
         }
+        // Create
         [HttpGet]
         public IActionResult Create()
         {
             PopulateDepartmentsDropDownList();
             return View();
         }
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                PopulateDepartmentsDropDownList(course.DepartmentID);
+            }
+            return RedirectToAction("Index");
+        }
+        
+        // Delete
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+            var courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+            return View(courses);
+        }
+        [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Courses == null)
+            {
+                return NotFound();
+            }
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
 
         private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
         {
@@ -32,5 +82,6 @@ namespace TallinnaRakenduslikKolledz.Controllers
                                    select d;
             ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
         }
+
     }
 }
